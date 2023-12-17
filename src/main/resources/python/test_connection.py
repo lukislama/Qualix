21f6f9cbdc7c27ce35a35fc5316caac3f317250e
Lukáš Sláma <slamalu2@fel.cvut.cz>
2023-12-17 14:56:44 +0100

    test_connection.py fix

--- src/main/resources/python/test_connection.py
+++ src/main/resources/python/test_connection.py
@@ -6,4 +6,4 @@ LAMP.connect(sys.argv[1], sys.argv[2], sys.argv[3])
 sempro = sys.argv[4]
 participants = LAMP.Participant.all_by_study(sempro)
 
-print(len(participants))
+print(len(participants['data']))
